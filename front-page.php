032a8fab4648dca4a7149d65f3f7569f4db09aa7
Dan Moutal <dan@moutal.ca>
2013-04-24 01:17:07 -0700

    front page with 3 loops. Missing the video loop
    Need to exclude categories on query_posts by slug. Also not sure if I need to call wp_reset_query() after the loops

--- front-page.php
+++ front-page.php
@@ -9,23 +9,48 @@
 get_header(); ?>
 
 	<div id="primary" class="content-area row">
-		<div id="content" class="site-content large-10 large-centered columns" role="main">
-
 		<?php if ( have_posts() ) : ?>
 
-			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
+		<div class="row">
+			<div id="lede" class="large-10 large-centered columns" role="main">
+				<?php /* Start the lede Loop */ 
+				query_posts( 'category_name=lede&showposts=1' );
+				while ( have_posts() ) : the_post();
+					$do_not_duplicate[] = $post->ID; ?>
+					<?php get_template_part( 'content', 'fplede' ); ?>
+				<?php endwhile; ?>
+			</div><!-- #lede -->
+		</div><!-- row -->
 
-				<?php
 
-					get_template_part( 'content', 'fplede' );
-				?>
+		<div class="row">
+			<div id="featured" class="large-10 large-centered columns" role="main">
+				<?php /* Start the featured Loop */ 
+				rewind_posts();
+				query_posts( 'category_name=featured&showposts=4' );
+				while ( have_posts() ) : the_post();
+					if (in_array($post->ID, $do_not_duplicate)) continue; ?>
+					<?php get_template_part( 'content', 'fp' ); ?>
+				<?php endwhile; ?>
+			</div><!-- #featured -->
+		</div><!-- row -->
 
-			<?php endwhile; ?>
 
-		<?php endif; ?>
 
-		</div><!-- #content -->
+		<div class="row">
+			<div id="beyond" class="large-10 large-centered columns" role="main">
+				<?php /* Start the featured Loop */ 
+				rewind_posts();
+				query_posts( 'category_name=beyond-planet-three&showposts=5' );
+				while ( have_posts() ) : the_post(); ?>
+					<?php get_template_part( 'content', 'fp' ); ?>
+				<?php endwhile; ?>
+			</div><!-- #featured -->
+		</div><!-- row -->
+
+
+
+	<?php endif; ?>
 	</div><!-- #primary -->
 
 <?php get_footer(); ?>
