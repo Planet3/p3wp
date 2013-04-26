4fcd7b0117001da2f409ed3980673b9004620dc2
Dan Moutal <dan@moutal.ca>
2013-04-26 16:31:27 -0700

    prevent loops from running if there are not posts

--- front-page.php
+++ front-page.php
@@ -10,59 +10,69 @@ get_header(); ?>
 
 	<div id="primary" class="content-area row">
 
+
+
+		<?php $args = array(
+			'category__in' => planet3_0_cat_slug_to_id('lede'),
+			'posts_per_page' => 1
+			);
+		/* Start the lede Loop */ 
+		$lede_query = new WP_Query( $args );
+		if ( $lede_query -> have_posts() ) : ?>
 			<div class="row">
 				<div id="lede" class="large-10 large-centered columns" role="main">
 					<h1>Lede</h1>
-					<?php $args = array(
-						'category__in' => planet3_0_cat_slug_to_id('lede'),
-						'posts_per_page' => 1
-						); ?>
-					<?php /* Start the lede Loop */ 
-					$lede_query = new WP_Query( $args );
-					while ( $lede_query -> have_posts() ) : $lede_query -> the_post();
+					<?php while ( $lede_query -> have_posts() ) : $lede_query -> the_post();
 						$do_not_duplicate[] = $post->ID; ?>
 						<?php get_template_part( 'content', 'fplede' ); ?>
 					<?php endwhile; ?>
 				</div><!-- #lede -->
 			</div><!-- row -->
-		<?php wp_reset_postdata(); ?>
+			<?php wp_reset_postdata(); ?>
+		<?php endif; ?>
 
-
+		<?php $args = array(
+			'category__in' => planet3_0_cat_slug_to_id('featured'),
+			'category__not_in' => planet3_0_cat_slug_to_id('media'),
+			'posts_per_page' => 4
+			);
+		/* Start the featured Loop */ 
+		$featured_query = new WP_Query( $args ); 
+		if ( $featured_query -> have_posts() ) : ?>
 			<div class="row">
 				<div id="featured" class="large-10 large-centered columns" role="main">
 					<h1>Featured</h1>
-					<?php $args = array(
-						'category__in' => planet3_0_cat_slug_to_id('featured'),
-						'category__not_in' => planet3_0_cat_slug_to_id('media'),
-						'posts_per_page' => 4
-						); ?>
-					<?php /* Start the featured Loop */ 
-					$featured_query = new WP_Query( $args );
-					while ( $featured_query -> have_posts() ) : $featured_query -> the_post();
-						if (in_array($post->ID, $do_not_duplicate)) continue; ?>
+					<?php while ( $featured_query -> have_posts() ) : $featured_query -> the_post();
+						if ( isset($do_not_duplicate) ) {
+							if (in_array($post->ID, $do_not_duplicate)) continue; 
+						} ?>
 						<?php get_template_part( 'content', 'fp' ); ?>
 					<?php endwhile; ?>
 				</div><!-- #featured -->
 			</div><!-- row -->
-		<?php wp_reset_postdata(); ?>
+			<?php wp_reset_postdata(); ?>
+		<?php endif; ?>
 
 
+
+
+		<?php $args = array(
+			'category__in' => planet3_0_cat_slug_to_id('beyond-planet-three'),
+			'posts_per_page' => 5
+			);
+		/* Start the beyond Loop */ 
+		$beyond_query = new WP_Query( $args );
+		if ( $beyond_query -> have_posts() ) : ?>
 			<div class="row">
 				<div id="beyond" class="large-10 large-centered columns" role="main">
 					<h1>Beyond Planet Three</h1>
-					<?php $args = array(
-						'category__in' => planet3_0_cat_slug_to_id('beyond-planet-three'),
-						'posts_per_page' => 5
-						); ?>
-					<?php /* Start the beyond Loop */ 
-					$beyond_query = new WP_Query( $args );
-					while ( $beyond_query -> have_posts() ) : $beyond_query -> the_post(); ?>
+					<?php while ( $beyond_query -> have_posts() ) : $beyond_query -> the_post(); ?>
 						<?php get_template_part( 'content', 'fp' ); ?>
 					<?php endwhile; ?>
 				</div><!-- #featured -->
 			</div><!-- row -->
-		<?php wp_reset_postdata(); ?>
-
+			<?php wp_reset_postdata(); ?>
+		<?php endif; ?>
 
 	</div><!-- #primary -->
 
