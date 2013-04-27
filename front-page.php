2bf7b6ac06fbd1c4867010ba71d5b67859fa74ec
Dan Moutal <dan@moutal.ca>
2013-04-26 17:11:56 -0700

    add sidebar

--- front-page.php
+++ front-page.php
@@ -29,47 +29,55 @@ get_header(); ?>
 			<?php wp_reset_postdata(); ?>
 		<?php endif; ?>
 
+			<div class="row" >
+				<div class="large-2 push-10 columns">
+					<?php get_sidebar(); ?>
+				</div><!-- large-2 large-offset10 columns -->
 
-		<?php $args = array(
-			'category__in' => planet3_0_cat_slug_to_id('featured'),
-			'category__not_in' => planet3_0_cat_slug_to_id('media'),
-			'posts_per_page' => 4
-			);
-		/* Start the featured Loop */ 
-		$featured_query = new WP_Query( $args ); 
-		if ( $featured_query -> have_posts() ) : ?>
-			<div id="featured" class="row">
-				<ul class="large-block-grid-3">
-					<h1>Featured</h1>
-					<?php while ( $featured_query -> have_posts() ) : $featured_query -> the_post();
-						if ( isset($do_not_duplicate) ) {
-							if (in_array($post->ID, $do_not_duplicate)) continue; 
-						} ?>
-						<?php get_template_part( 'content', 'fp' ); ?>
-					<?php endwhile; ?>
-				</ul><!-- large-block-grid-3 -->
-			</div><!-- #featured .row -->
-			<?php wp_reset_postdata(); ?>
-		<?php endif; ?>
+				<div class="large-10 pull-2 columns">
+					<?php $args = array(
+						'category__in' => planet3_0_cat_slug_to_id('featured'),
+						'category__not_in' => planet3_0_cat_slug_to_id('media'),
+						'posts_per_page' => 4
+						);
+					/* Start the featured Loop */ 
+					$featured_query = new WP_Query( $args ); 
+					if ( $featured_query -> have_posts() ) : ?>
+						<div id="featured" class="row">
+							<ul class="large-block-grid-3">
+								<h1>Featured</h1>
+								<?php while ( $featured_query -> have_posts() ) : $featured_query -> the_post();
+									if ( isset($do_not_duplicate) ) {
+										if (in_array($post->ID, $do_not_duplicate)) continue; 
+									} ?>
+									<?php get_template_part( 'content', 'fp' ); ?>
+								<?php endwhile; ?>
+							</ul><!-- large-block-grid-3 -->
+						</div><!-- #featured .row -->
+						<?php wp_reset_postdata(); ?>
+					<?php endif; ?>
 
 
-		<?php $args = array(
-			'category__in' => planet3_0_cat_slug_to_id('beyond-planet-three'),
-			'posts_per_page' => 5
-			);
-		/* Start the beyond Loop */ 
-		$beyond_query = new WP_Query( $args );
-		if ( $beyond_query -> have_posts() ) : ?>
-			<div id="beyond" class="row">
-				<ul class="large-block-grid-3">
-					<h1>Beyond Planet Three</h1>
-					<?php while ( $beyond_query -> have_posts() ) : $beyond_query -> the_post(); ?>
-						<?php get_template_part( 'content', 'fp' ); ?>
-					<?php endwhile; ?>
-				</ul><!-- .large-block-grid-3 -->
-			</div><!-- # beyond .row -->
-			<?php wp_reset_postdata(); ?>
-		<?php endif; ?>
+					<?php $args = array(
+						'category__in' => planet3_0_cat_slug_to_id('beyond-planet-three'),
+						'posts_per_page' => 6
+						);
+					/* Start the beyond Loop */ 
+					$beyond_query = new WP_Query( $args );
+					if ( $beyond_query -> have_posts() ) : ?>
+						<div id="beyond" class="row">
+							<ul class="large-block-grid-3">
+								<h1>Beyond Planet Three</h1>
+								<?php while ( $beyond_query -> have_posts() ) : $beyond_query -> the_post(); ?>
+									<?php get_template_part( 'content', 'fp' ); ?>
+								<?php endwhile; ?>
+							</ul><!-- .large-block-grid-3 -->
+						</div><!-- # beyond .row -->
+						<?php wp_reset_postdata(); ?>
+					<?php endif; ?>
+				</div><!-- large-10 pull-2 -->
+
+			</div><!-- row -->
 
 	</div><!-- #primary -->
 
