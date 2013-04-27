e6d8431685e848b512ecfeabd1f3d8d81bcc8989
Dan Moutal <dan@moutal.ca>
2013-04-27 01:11:34 -0700

    Move section headers out of block-grid and push sidebar to the bottom in source order

--- front-page.php
+++ front-page.php
@@ -9,75 +9,73 @@
 get_header(); ?>
 
 	<div id="primary" class="content-area row" role="main">
+			<?php $args = array(
+				'category__in' => planet3_0_cat_slug_to_id('lede'),
+				'posts_per_page' => 1
+				);
+			/* Start the lede Loop */ 
+			$lede_query = new WP_Query( $args );
+			if ( $lede_query -> have_posts() ) : ?>
+				<div class="row">
+					<div id="lede" class="large-12 columns">
+						<h1>Lede</h1>
+						<?php while ( $lede_query -> have_posts() ) : $lede_query -> the_post();
+							$do_not_duplicate[] = $post->ID; ?>
+							<?php get_template_part( 'content', 'fplede' ); ?>
+						<?php endwhile; ?>
+					</div><!-- #lede -->
+				</div><!-- row -->
+				<?php wp_reset_postdata(); ?>
+			<?php endif; ?>
 
-		<?php $args = array(
-			'category__in' => planet3_0_cat_slug_to_id('lede'),
-			'posts_per_page' => 1
-			);
-		/* Start the lede Loop */ 
-		$lede_query = new WP_Query( $args );
-		if ( $lede_query -> have_posts() ) : ?>
-			<div class="row">
-				<div id="lede" class="large-12 columns">
-					<h1>Lede</h1>
-					<?php while ( $lede_query -> have_posts() ) : $lede_query -> the_post();
-						$do_not_duplicate[] = $post->ID; ?>
-						<?php get_template_part( 'content', 'fplede' ); ?>
-					<?php endwhile; ?>
-				</div><!-- #lede -->
-			</div><!-- row -->
-			<?php wp_reset_postdata(); ?>
-		<?php endif; ?>
-
-			<div class="row" >
-				<div class="large-2 push-10 columns">
-					<?php get_sidebar(); ?>
-				</div><!-- large-2 large-offset10 columns -->
-
-				<div class="large-10 pull-2 columns">
-					<?php $args = array(
-						'category__in' => planet3_0_cat_slug_to_id('featured'),
-						'category__not_in' => planet3_0_cat_slug_to_id('media'),
-						'posts_per_page' => 4
-						);
-					/* Start the featured Loop */ 
-					$featured_query = new WP_Query( $args ); 
-					if ( $featured_query -> have_posts() ) : ?>
-						<div id="featured" class="row">
-							<ul class="large-block-grid-3">
-								<h1>Featured</h1>
-								<?php while ( $featured_query -> have_posts() ) : $featured_query -> the_post();
-									if ( isset($do_not_duplicate) ) {
-										if (in_array($post->ID, $do_not_duplicate)) continue; 
-									} ?>
-									<?php get_template_part( 'content', 'fp' ); ?>
-								<?php endwhile; ?>
-							</ul><!-- large-block-grid-3 -->
-						</div><!-- #featured .row -->
-						<?php wp_reset_postdata(); ?>
-					<?php endif; ?>
+		<div class="row" >
+			<div class="large-9 columns">
+				<?php $args = array(
+					'category__in' => planet3_0_cat_slug_to_id('featured'),
+					'category__not_in' => planet3_0_cat_slug_to_id('media'),
+					'posts_per_page' => 4
+					);
+				/* Start the featured Loop */ 
+				$featured_query = new WP_Query( $args ); 
+				if ( $featured_query -> have_posts() ) : ?>
+					<div id="featured" class="row">
+						<h1>Featured</h1>
+						<ul class="large-block-grid-3">
+							<?php while ( $featured_query -> have_posts() ) : $featured_query -> the_post();
+								if ( isset($do_not_duplicate) ) {
+									if (in_array($post->ID, $do_not_duplicate)) continue; 
+								} ?>
+								<?php get_template_part( 'content', 'fp' ); ?>
+							<?php endwhile; ?>
+						</ul><!-- large-block-grid-3 -->
+					</div><!-- #featured row -->
+					<?php wp_reset_postdata(); ?>
+				<?php endif; ?>
 
 
-					<?php $args = array(
-						'category__in' => planet3_0_cat_slug_to_id('beyond-planet-three'),
-						'posts_per_page' => 6
-						);
-					/* Start the beyond Loop */ 
-					$beyond_query = new WP_Query( $args );
-					if ( $beyond_query -> have_posts() ) : ?>
-						<div id="beyond" class="row">
-							<ul class="large-block-grid-3">
-								<h1>Beyond Planet Three</h1>
-								<?php while ( $beyond_query -> have_posts() ) : $beyond_query -> the_post(); ?>
-									<?php get_template_part( 'content', 'fp' ); ?>
-								<?php endwhile; ?>
-							</ul><!-- .large-block-grid-3 -->
-						</div><!-- # beyond .row -->
-						<?php wp_reset_postdata(); ?>
-					<?php endif; ?>
-				</div><!-- large-10 pull-2 -->
+				<?php $args = array(
+					'category__in' => planet3_0_cat_slug_to_id('beyond-planet-three'),
+					'posts_per_page' => 6
+					);
+				/* Start the beyond Loop */ 
+				$beyond_query = new WP_Query( $args );
+				if ( $beyond_query -> have_posts() ) : ?>
+					<div id="beyond" class="row">
+					<h1>Beyond Planet Three</h1>
+						<ul class="large-block-grid-3">
+							<?php while ( $beyond_query -> have_posts() ) : $beyond_query -> the_post(); ?>
+								<?php get_template_part( 'content', 'fp' ); ?>
+							<?php endwhile; ?>
+						</ul><!-- .large-block-grid-3 -->
+					</div><!-- # beyond row -->
+					<?php wp_reset_postdata(); ?>
+				<?php endif; ?>
+			</div><!-- large-9 -->
 
-			</div><!-- row -->
+			<div class="large-3 columns">
+				<?php get_sidebar(); ?>
+			</div><!-- large-3 -->
+		</div><!-- row -->
 
 	</div><!-- #primary -->
 
