df1a70106d62832d183a223a893886530bc2c727
Dan Moutal <dan@moutal.ca>
2013-05-02 14:09:17 -0700

    fix for overflowing video embeds on front page

--- content-fpmedia.php
+++ content-fpmedia.php
@@ -6,31 +6,31 @@
 ?>
 
 <li>
-    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-        <header class="entry-header">
-            <h1 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'planet3_0' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
+		<header class="entry-header">
+			<h1 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'planet3_0' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
 
-            <?php if ( 'post' == get_post_type() ) : ?>
-            <div class="entry-meta">
-                <p><?php planet3_0_posted_on(); ?> by <?php planet3_0_posted_by(); ?></p>
-            </div><!-- .entry-meta -->
-            <?php endif; ?>
-        </header><!-- .entry-header -->
+			<?php if ( 'post' == get_post_type() ) : ?>
+			<div class="entry-meta">
+				<p><?php planet3_0_posted_on(); ?> by <?php planet3_0_posted_by(); ?></p>
+			</div><!-- .entry-meta -->
+			<?php endif; ?>
+		</header><!-- .entry-header -->
 
-        <div class="entry-summary">
-            <?php the_content(); ?>
-        </div><!-- .entry-summary -->
+		<div class="entry-summary">
+			<?php the_content(); ?>
+		</div><!-- .entry-summary -->
 
-        <div class="row">
-            <footer class="entry-meta meta-bellow large-12 columns">
+		<div class="row">
+			<footer class="entry-meta meta-bellow large-12 columns">
 
-                <?php if ( ! post_password_required() && ( comments_open() || '0' != get_comments_number() ) ) : ?>
-                <span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'planet3_0' ), __( '1 Comment', 'planet3_0' ), __( '% Comments', 'planet3_0' ) ); ?></span>
-                <?php endif; ?>
+				<?php if ( ! post_password_required() && ( comments_open() || '0' != get_comments_number() ) ) : ?>
+				<span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'planet3_0' ), __( '1 Comment', 'planet3_0' ), __( '% Comments', 'planet3_0' ) ); ?></span>
+				<?php endif; ?>
 
-                <?php edit_post_link( __( 'Edit', 'planet3_0' ), '<span class="sep"> | </span><span class="edit-link">', '</span>' ); ?>
-            </footer><!-- .entry-meta -->
-        </div><!-- .row -->
-    </article><!-- #post-## -->
+				<?php edit_post_link( __( 'Edit', 'planet3_0' ), '<span class="sep"> | </span><span class="edit-link">', '</span>' ); ?>
+			</footer><!-- .entry-meta -->
+		</div><!-- .row -->
+	</article><!-- #post-## -->
 </li>
