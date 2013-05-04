321b9f88e7c609cad0b3e963b168a4d572224f73
Dan Moutal <dan@moutal.ca>
2013-05-04 16:52:26 -0700

    small entry-meta tweak

--- content-fplede.php
+++ content-fplede.php
@@ -24,10 +24,8 @@
 
 		<div class="entry-summary large-6 columns">
 			<?php the_excerpt(); ?>
-		</div><!-- .entry-summary -->
 
-		<div class="row">
-			<footer class="entry-meta meta-bellow large-6 columns">
+			<footer class="entry-meta meta-bellow">
 
 				<?php if ( ! post_password_required() && ( comments_open() || '0' != get_comments_number() ) ) : ?>
 				<span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'planet3_0' ), __( '1 Comment', 'planet3_0' ), __( '% Comments', 'planet3_0' ) ); ?></span>
@@ -35,7 +33,7 @@
 
 				<?php edit_post_link( __( 'Edit', 'planet3_0' ), '<span class="sep"> | </span><span class="edit-link">', '</span>' ); ?>
 			</footer><!-- .entry-meta -->
-	</div><!-- row -->
+		</div><!-- .entry-summary -->
 
 
 	</div><!-- row -->
