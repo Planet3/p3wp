48763e5eb8a352d74b4bd68236d1a1e479e21cc6
Dan Moutal <dan@moutal.ca>
2013-05-08 14:39:22 -0700

    Front Page redesign test

--- content-fpbeyond.php
+++ content-fpbeyond.php
@@ -8,14 +8,13 @@
 <li>
 	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-		<?php the_post_thumbnail(); ?> 
-
 		<header class="entry-header">
-			<h1 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'planet3_0' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
+			<h2 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'planet3_0' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
 
 		</header><!-- .entry-header -->
 
 		<div class="entry-summary">
+			<?php the_post_thumbnail(); ?> 
 			<?php the_excerpt(); ?>
 		</div><!-- .entry-summary -->
 
--- front-page.php
+++ front-page.php
@@ -28,19 +28,19 @@ get_header(); ?>
 				<?php wp_reset_postdata(); ?>
 			<?php endif; ?>
 
-		<div class="row" >
-			<div class="posts-area large-9 columns">
+		<div class="row">
+			<div class="posts-area large-5 columns">
 
 				<?php $args = array(
 					'category__in' => planet3_0_cat_slug_to_id('featured'),
 					'category__not_in' => planet3_0_cat_slug_to_id('media'),
-					'posts_per_page' => 4
+					'posts_per_page' => 3
 					);
 				/* Start the featured Loop */ 
 				$featured_query = new WP_Query( $args ); 
 				if ( $featured_query -> have_posts() ) : ?>
 					<div id="featured" >
-						<ul class="large-block-grid-3">
+						<ul class="large-block-grid-2">
 							<?php while ( $featured_query -> have_posts() ) : $featured_query -> the_post();
 								if ( isset($do_not_duplicate) ) {
 									if (in_array($post->ID, $do_not_duplicate)) continue; 
@@ -59,44 +59,51 @@ get_header(); ?>
 
 
 
-			<div id="media">
-				<ul class="large-block-grid-2">
+				<div id="media">
+					<ul class="large-block-grid-1">
 
-					<?php $args = array(
-						'category__in' => planet3_0_cat_slug_to_id('video'),
-						'posts_per_page' => 1
-						);
-					/* Start the video media Loop */ 
-					$video_query = new WP_Query( $args );
-					if ( $video_query -> have_posts() ) : ?>
-						<?php while ( $video_query -> have_posts() ) : $video_query -> the_post(); ?>
-							<?php get_template_part( 'content', 'fpmedia' ); ?>
-						<?php endwhile; ?>
-						<?php wp_reset_postdata(); ?>
-					<?php endif; ?>
+						<?php $args = array(
+							'category__in' => planet3_0_cat_slug_to_id('video'),
+							'posts_per_page' => 1
+							);
+						/* Start the video media Loop */ 
+						$video_query = new WP_Query( $args );
+						if ( $video_query -> have_posts() ) : ?>
+							<?php while ( $video_query -> have_posts() ) : $video_query -> the_post(); ?>
+								<?php get_template_part( 'content', 'fpmedia' ); ?>
+							<?php endwhile; ?>
+							<div class="row">
+								<div class="archive-button large-12 columns">
+									<a class="radius small button" href="<?php echo get_category_link( $args['category__in'] ); ?>"><?php echo esc_html( get_the_category_by_ID( $args['category__in'] ) ); ?> Archives</a>
+								</div><!-- .archive-button -->
+							</div><!-- row -->
+							<?php wp_reset_postdata(); ?>
+						<?php endif; ?>
 
-					<?php $args = array(
-						'category__in' => planet3_0_cat_slug_to_id('image'),
-						'posts_per_page' => 1
-						);
-					/* Start the image media Loop */ 
-					$image_query = new WP_Query( $args );
-					if ( $image_query -> have_posts() ) : ?>
-						<?php while ( $image_query -> have_posts() ) : $image_query -> the_post(); ?>
-							<?php get_template_part( 'content', 'fpmedia' ); ?>
-						<?php endwhile; ?>
-						<?php wp_reset_postdata(); ?>
-						<div class="row">
-							<div class="archive-button large-12 columns">
-								<a class="radius small button" href="<?php echo get_category_link( $args['category__in'] ); ?>"><?php echo esc_html( get_the_category_by_ID( $args['category__in'] ) ); ?> Archives</a>
-							</div><!-- .archive-button -->
-						</div><!-- row -->
-					<?php endif; ?>
+						<?php $args = array(
+							'category__in' => planet3_0_cat_slug_to_id('image'),
+							'posts_per_page' => 1
+							);
+						/* Start the image media Loop */ 
+						$image_query = new WP_Query( $args );
+						if ( $image_query -> have_posts() ) : ?>
+							<?php while ( $image_query -> have_posts() ) : $image_query -> the_post(); ?>
+								<?php get_template_part( 'content', 'fpmedia' ); ?>
+							<?php endwhile; ?>
+							<div class="row">
+								<div class="archive-button large-12 columns">
+									<a class="radius small button" href="<?php echo get_category_link( $args['category__in'] ); ?>"><?php echo esc_html( get_the_category_by_ID( $args['category__in'] ) ); ?> Archives</a>
+								</div><!-- .archive-button -->
+							</div><!-- row -->
+							<?php wp_reset_postdata(); ?>
+						<?php endif; ?>
 
-				</ul><!-- .large-block-grid-3 -->
-			</div><!-- #media -->
+					</ul><!-- .large-block-grid-3 -->
+				</div><!-- #media -->
 
+			</div><!-- posts-area large-5 -->
 
+			<div class="posts-area large-4 columns">
 				<?php $args = array(
 					'category__in' => planet3_0_cat_slug_to_id('beyond-planet-three'),
 					'posts_per_page' => 6
@@ -109,7 +116,7 @@ get_header(); ?>
 							<h1><?php echo esc_html( get_the_category_by_ID( $args['category__in'] ) ); ?></h1>
 							<?php echo category_description( $args['category__in'] ); ?>
 						</div><!-- section-title -->
-						<ul class="large-block-grid-3">
+						<ul class="large-block-grid-1">
 							<?php while ( $beyond_query -> have_posts() ) : $beyond_query -> the_post(); ?>
 								<?php get_template_part( 'content', 'fpbeyond' ); ?>
 							<?php endwhile; ?>
@@ -122,8 +129,7 @@ get_header(); ?>
 					</div><!-- #beyond -->
 					<?php wp_reset_postdata(); ?>
 				<?php endif; ?>
-
-			</div><!-- posts-area large-9 -->
+			</div><!-- posts-area large-4 columns -->
 
 			<div class="large-3 columns hide-for-small">
 				<?php get_sidebar(); ?>
