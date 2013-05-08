2ea5c41115a5edfbcf5613dc34d7a398bbf7c07e
Dan Moutal <dan@moutal.ca>
2013-05-07 17:29:09 -0700

    function clean up

--- functions.php
+++ functions.php
@@ -78,10 +78,10 @@ function planet3_0_setup() {
 function foundation_top_bar() {
 	wp_nav_menu(array( 
 		'container' => false,                           // remove nav container
-		'container_class' => 'menu',           		// class of container
-		'menu' => '',                      	        // menu name
-		'menu_class' => 'top-bar-menu left',         	// adding custom nav class
-		'theme_location' => 'top-bar-l',                // where it's located in the theme
+		'container_class' => 'menu',                    // class of container
+		'menu' => '',                                   // menu name
+		'menu_class' => 'top-bar-menu',                 // adding custom nav class
+		'theme_location' => 'top-bar',                  // where it's located in the theme
 		'before' => '',                                 // before each link <a> 
 		'after' => '',                                  // after each link </a>
 		'link_before' => '',                            // before each link text
