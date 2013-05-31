280693f11215b17318c7c77bc4d18ff7dd85cf86
Dan Moutal <dan@moutal.ca>
2013-05-30 23:59:17 -0700

    more tweaks

--- content-fpfeatured.php
+++ content-fpfeatured.php
@@ -20,7 +20,7 @@
 			<div class="large-12 small-8 columns">
 				<header class="entry-header">
 					<h2 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( 'Permalink to %s', the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
-				<div class="entry-meta meta-above">
+				<div class="entry-meta meta-above hide-for-small">
 					<p>By <?php planet3_0_posted_by(); ?></p>
 				</div><!-- .entry-meta -->
 				</header><!-- .entry-header -->
