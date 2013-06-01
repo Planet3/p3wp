5a53bd47047e843c406465bbfc1e3a3377025ca4
Dan Moutal <dan@moutal.ca>
2013-06-01 02:03:26 -0700

    fix tabs

--- content-fpquote.php
+++ content-fpquote.php
@@ -6,21 +6,21 @@
 ?>
 
 <li>
-    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-        <header class="entry-header assistive-text">
-            <h2 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( 'Permalink to %s', the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
-        </header><!-- .entry-header -->
+	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+		<header class="entry-header assistive-text">
+			<h2 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( 'Permalink to %s', the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
+		</header><!-- .entry-header -->
 
-        <div class="entry-summary">
-            <?php if( $post->post_excerpt ) {
-                the_excerpt();
-            } else {
-                the_content();
-            } ?>
-        </div><!-- .entry-summary -->
+		<div class="entry-summary">
+			<?php if( $post->post_excerpt ) {
+				the_excerpt();
+			} else {
+				the_content();
+			} ?>
+		</div><!-- .entry-summary -->
 
-        <footer class="entry-meta meta-bellow">
-            <?php edit_post_link( 'Edit', '<span class="edit-link">', '</span>' ); ?>
-        </footer><!-- .entry-meta -->
-    </article><!-- #post-## -->
+		<footer class="entry-meta meta-bellow">
+			<?php edit_post_link( 'Edit', '<span class="edit-link">', '</span>' ); ?>
+		</footer><!-- .entry-meta -->
+	</article><!-- #post-## -->
 </li>
