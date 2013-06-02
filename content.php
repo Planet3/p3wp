ac32803b636d5eea599c738ce2312d3fc2e9cab4
Dan Moutal <dan@moutal.ca>
2013-06-01 21:29:43 -0700

    Display full content in archive pages for posts in the media category or descendants

--- content.php
+++ content.php
@@ -16,7 +16,7 @@
 		<?php endif; ?>
 	</header><!-- .entry-header -->
 
-	<?php if ( is_search() || is_author() ) : // Only display Excerpts for Search and Author pages?>
+	<?php if ( ! in_category( planet3_0_cat_slug_to_id('media') ) && ! planet3_0_post_is_in_descendant_category( planet3_0_cat_slug_to_id('media') ) && is_archive() || is_search() || is_author()  ) : // Only display Excerpts for Search and Author pages?>
 	<div class="entry-summary">
 		<?php the_excerpt(); ?>
 	</div><!-- .entry-summary -->
@@ -30,16 +30,12 @@
 	<div class="row">
 		<footer class="entry-meta meta-bellow large-12 columns">
 			<?php if ( 'post' == get_post_type() ) : // Hide category and tag text for pages on Search ?>
-
-			<?php planet3_0_posted_in(); ?>
-
+				<?php planet3_0_posted_in(); ?>
 			<?php endif; // End if 'post' == get_post_type() ?>
-
 			<?php if ( ! post_password_required() && ( comments_open() || '0' != get_comments_number() ) ) : ?>
-			<span class="sep"> | </span>
-			<span class="comments-link"><?php comments_popup_link( 'Leave a comment', '1 Comment', '% Comments' ); ?></span>
+				<span class="sep"> | </span>
+				<span class="comments-link"><?php comments_popup_link( 'Leave a comment', '1 Comment', '% Comments' ); ?></span>
 			<?php endif; ?>
-
 			<?php edit_post_link( 'Edit', '<span class="sep"> | </span><span class="edit-link">', '</span>' ); ?>
 		</footer><!-- .entry-meta -->
 	</div><!-- .row -->
