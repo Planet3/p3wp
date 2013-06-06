b3b2e7747d1a0ce82eb9ed28a76e35ffbc87e365
Dan Moutal <dan@moutal.ca>
2013-06-06 01:35:25 -0700

    Fix layout on short posts with long bio

--- comments.php
+++ comments.php
@@ -22,59 +22,57 @@
 		return;
 ?>
 
-<div class="row">
-	<div id="comments" class="comments-area large-10 large-offset-2 columns">
+<div id="comments" class="comments-area large-10 columns">
 
-		<?php // You can start editing here -- including this comment! ?>
+	<?php // You can start editing here -- including this comment! ?>
 
-		<?php if ( have_comments() ) : ?>
-			<h2 class="comments-title">
-				<?php
-					printf( 'One comment', '%1$s comments', get_comments_number(), 'comments title',
-						number_format_i18n( get_comments_number() ), '<span>' . get_the_title() . '</span>' );
-				?>
-			</h2>
+	<?php if ( have_comments() ) : ?>
+		<h2 class="comments-title">
+			<?php
+				printf( _nx( 'One comment on &ldquo;%2$s&rdquo;', '%1$s comments on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title' ),
+					number_format_i18n( get_comments_number() ), '<span>' . get_the_title() . '</span>' );
+			?>
+		</h2>
 
-			<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through ?>
-			<nav id="comment-nav-above" class="navigation-comment" role="navigation">
-				<h1 class="screen-reader-text">Comment navigation</h1>
-				<div class="previous"><?php previous_comments_link( '&larr; Older Comments' ); ?></div>
-				<div class="next"><?php next_comments_link( 'Newer Comments &rarr;' ); ?></div>
-			</nav><!-- #comment-nav-before -->
-			<?php endif; // check for comment navigation ?>
+		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through ?>
+		<nav id="comment-nav-above" class="navigation-comment" role="navigation">
+			<h1 class="screen-reader-text">Comment navigation</h1>
+			<div class="previous"><?php previous_comments_link( '&larr; Older Comments' ); ?></div>
+			<div class="next"><?php next_comments_link( 'Newer Comments &rarr;' ); ?></div>
+		</nav><!-- #comment-nav-before -->
+		<?php endif; // check for comment navigation ?>
 
-			<ol class="comment-list">
-				<?php
-					/* Loop through and list the comments. Tell wp_list_comments()
-					 * to use planet3_0_comment() to format the comments.
-					 * If you want to overload this in a child theme then you can
-					 * define planet3_0_comment() and that will be used instead.
-					 * See planet3_0_comment() in inc/template-tags.php for more.
-					 */
-					wp_list_comments( array( 'callback' => 'planet3_0_comment' ) );
-				?>
-			</ol><!-- .comment-list -->
+		<ol class="comment-list">
+			<?php
+				/* Loop through and list the comments. Tell wp_list_comments()
+				 * to use planet3_0_comment() to format the comments.
+				 * If you want to overload this in a child theme then you can
+				 * define planet3_0_comment() and that will be used instead.
+				 * See planet3_0_comment() in inc/template-tags.php for more.
+				 */
+				wp_list_comments( array( 'callback' => 'planet3_0_comment' ) );
+			?>
+		</ol><!-- .comment-list -->
 
-			<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through ?>
-			<nav id="comment-nav-below" class="navigation-comment" role="navigation">
-				<h1 class="screen-reader-text">Comment navigation</h1>
-				<div class="previous"><?php previous_comments_link( '&larr; Older Comments' ); ?></div>
-				<div class="next"><?php next_comments_link( 'Newer Comments &rarr;' ); ?></div>
-			</nav><!-- #comment-nav-below -->
-			<?php endif; // check for comment navigation ?>
+		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through ?>
+		<nav id="comment-nav-below" class="navigation-comment" role="navigation">
+			<h1 class="screen-reader-text">Comment navigation</h1>
+			<div class="previous"><?php previous_comments_link( '&larr; Older Comments' ); ?></div>
+			<div class="next"><?php next_comments_link( 'Newer Comments &rarr;' ); ?></div>
+		</nav><!-- #comment-nav-below -->
+		<?php endif; // check for comment navigation ?>
 
-		<?php endif; // have_comments() ?>
+	<?php endif; // have_comments() ?>
 
-		<?php
-			// If comments are closed and there are comments, let's leave a little note, shall we?
-			if ( ! comments_open() && '0' != get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) :
-		?>
-			<p class="no-comments">Comments are closed.</p>
-		<?php endif; ?>
+	<?php
+		// If comments are closed and there are comments, let's leave a little note, shall we?
+		if ( ! comments_open() && '0' != get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) :
+	?>
+		<p class="no-comments">Comments are closed.</p>
+	<?php endif; ?>
 
-		<?php get_sidebar( 'comments' ); ?>
+	<?php get_sidebar( 'comments' ); ?>
 
-		<?php comment_form(); ?>
+	<?php comment_form(); ?>
 
-	</div><!-- #comments -->
-</div><!-- .row -->
+</div><!-- #comments -->
--- content-single.php
+++ content-single.php
@@ -10,39 +10,33 @@
 		<h1 class="entry-title"><?php the_title(); ?></h1>
 	</header><!-- .entry-header -->
 
-	<div class="row">
-		<div class="byline large-2 columns">
-			<div class="entry-meta meta-above">
-				<?php planet3_0_posted_on(); ?>
-			</div><!-- .entry-meta -->
-			<div class="author-avatar hide-for-small">
-				<?php if ( validate_gravatar( get_the_author_meta( 'user_email' ) ) ) :
-					echo get_avatar( get_the_author_meta( 'user_email' ), 256, $default, get_the_author() ); 
-				endif; ?>
-			</div>
-			<h1><?php planet3_0_posted_by(); ?></h1>
-				<?php
-				// Display Author's discription if it exists
-				if ( get_the_author_meta( 'user_description' ) ) : ?>
-			<div class="author-bio hide-for-small">
-				<p><?php the_author_meta( 'user_description' ); ?></p>
-			</div><!-- .author-bio -->
-			<?php endif; ?>
 
-		</div><!-- .byline -->
+	<div class="byline large-2 columns">
+		<div class="entry-meta meta-above">
+			<?php planet3_0_posted_on(); ?> <?php edit_post_link( 'Edit', '<span class="edit-link">', '</span>' ); ?>
+		</div><!-- .entry-meta -->
+		<div class="author-avatar hide-for-small">
+			<?php if ( validate_gravatar( get_the_author_meta( 'user_email' ) ) ) :
+				echo get_avatar( get_the_author_meta( 'user_email' ), 256, $default, get_the_author() ); 
+			endif; ?>
+		</div>
+		<h1><?php planet3_0_posted_by(); ?></h1>
+			<?php
+			// Display Author's discription if it exists
+			if ( get_the_author_meta( 'user_description' ) ) : ?>
+		<div class="author-bio hide-for-small">
+			<p><?php the_author_meta( 'user_description' ); ?></p>
+		</div><!-- .author-bio -->
+		<?php endif; ?>
+	</div><!-- .byline -->
 
-		<div class="large-10 columns">
-			<div class="entry-content">
-				<?php the_content(); ?>
-				<?php wp_link_pages( array( 'before' => '<div class="page-links">' . 'Pages:', 'after' => '</div>' ) ); ?>
-			</div><!-- .entry-content -->
-			<div class="row">
-				<footer class="entry-meta meta-bellow large-12 columns" >
-					<?php planet3_0_posted_in(); ?>
-					<?php edit_post_link( 'Edit', '<span class="edit-link">', '</span>' ); ?>
-				</footer><!-- .entry-meta -->
-			</div><!-- .row -->
-		</div><!-- . large-9 columns -->
-	</div><!-- .row -->
-
+	<div class="large-10 columns">
+		<div class="entry-content">
+			<?php the_content(); ?>
+			<?php wp_link_pages( array( 'before' => '<div class="page-links">' . 'Pages:', 'after' => '</div>' ) ); ?>
+		</div><!-- .entry-content -->
+		<footer class="entry-meta meta-bellow large-12 columns" >
+			<?php planet3_0_posted_in(); ?>
+		</footer><!-- .entry-meta -->
+	</div><!-- . large-9 columns -->
 </article><!-- #post-## -->
