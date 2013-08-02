2aa3a5b0c6aa14223986bd04c11b1e554396a1fb
Dan Moutal <dan@moutal.ca>
2013-08-02 13:54:55 -0700

    widget re-written from scratch. no longer breaks site, but doesn't do anything yet

--- inc/widgets.php
+++ inc/widgets.php
@@ -5,109 +5,38 @@
  *
  * @since Planet3.0 3.0
  */
-class Planet3_0_Recent_Comments extends WP_Widget {
+
+class Planet3_0_Recent_Comments_Widget extends WP_Widget {
 
 	function __construct() {
-		$widget_ops = array('classname' => 'widget_recent_comments', 'description' => __( 'The most recent comments' ) );
-		parent::__construct('p3-recent-comments', __('P3 Recent Comments'), $widget_ops);
-		$this->alt_option_name = 'widget_recent_comments';
-
-		if ( is_active_widget(false, false, $this->id_base) )
-			add_action( 'wp_head', array($this, 'recent_comments_style') );
-
-		add_action( 'comment_post', array($this, 'flush_widget_cache') );
-		add_action( 'transition_comment_status', array($this, 'flush_widget_cache') );
+		parent:: __construct( false, $name = 'P3 Recent Comments' );
 	}
 
-	function recent_comments_style() {
-		if ( ! current_theme_supports( 'widgets' ) // Temp hack #14876
-			|| ! apply_filters( 'show_recent_comments_widget_style', true, $this->id_base ) )
-			return;
+	function form() {
+
+	}
+
+	function update() {
+		
+	}
+
+	function widget( $args, $instance) {
 		?>
-	<style type="text/css">.recentcomments a{display:inline !important;padding:0 !important;margin:0 !important;}</style>
-<?php
+		<li class="widget widget_recent_comments p3_widget_recent_comments">
+			<h1 class="widget-title"> Recent Comments</h1>
+			<ul>
+				<li class="recentcomments">hello!</li>
+			</ul>
+		</li>
+		<?php
 	}
 
-	function flush_widget_cache() {
-		wp_cache_delete('widget_recent_comments', 'widget');
-	}
-
-	function widget( $args, $instance ) {
-		global $comments, $comment;
-
-		$cache = wp_cache_get('widget_recent_comments', 'widget');
-
-		if ( ! is_array( $cache ) )
-			$cache = array();
-
-		if ( ! isset( $args['widget_id'] ) )
-			$args['widget_id'] = $this->id;
-
-		if ( isset( $cache[ $args['widget_id'] ] ) ) {
-			echo $cache[ $args['widget_id'] ];
-			return;
-		}
-
-		extract($args, EXTR_SKIP);
-		$output = '';
-
-		$title = ( ! empty( $instance['title'] ) ) ? $instance['title'] : __( 'Recent Comments' );
-		$title = apply_filters( 'widget_title', $title, $instance, $this->id_base );
-		$number = ( ! empty( $instance['number'] ) ) ? absint( $instance['number'] ) : 5;
-		if ( ! $number )
-			$number = 5;
-
-		$comments = get_comments( apply_filters( 'widget_comments_args', array( 'number' => $number, 'status' => 'approve', 'post_status' => 'publish' ) ) );
-		$output .= $before_widget;
-		if ( $title )
-			$output .= $before_title . $title . $after_title;
-
-		$output .= '<ul id="recentcomments">';
-		if ( $comments ) {
-			// Prime cache for associated posts. (Prime post term cache if we need it for permalinks.)
-			$post_ids = array_unique( wp_list_pluck( $comments, 'comment_post_ID' ) );
-			_prime_post_caches( $post_ids, strpos( get_option( 'permalink_structure' ), '%category%' ), false );
-
-			foreach ( (array) $comments as $comment) {
-				$output .=  '<li class="recentcomments">' . /* translators: comments widget: 1: comment author, 2: post link */ sprintf(_x('%1$s on %2$s', 'widgets'), get_comment_author_link(), '<a href="' . esc_url( get_comment_link($comment->comment_ID) ) . '">' . get_the_title($comment->comment_post_ID) . '</a>') . '</li>';
-			}
-		}
-		$output .= '</ul>';
-		$output .= $after_widget;
-
-		echo $output;
-		$cache[$args['widget_id']] = $output;
-		wp_cache_set('widget_recent_comments', $cache, 'widget');
-	}
-
-	function update( $new_instance, $old_instance ) {
-		$instance = $old_instance;
-		$instance['title'] = strip_tags($new_instance['title']);
-		$instance['number'] = absint( $new_instance['number'] );
-		$this->flush_widget_cache();
-
-		$alloptions = wp_cache_get( 'alloptions', 'options' );
-		if ( isset($alloptions['widget_recent_comments']) )
-			delete_option('widget_recent_comments');
-
-		return $instance;
-	}
-
-	function form( $instance ) {
-		$title  = isset( $instance['title'] ) ? esc_attr( $instance['title'] ) : '';
-		$number = isset( $instance['number'] ) ? absint( $instance['number'] ) : 5;
-?>
-		<p><label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label>
-		<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo $title; ?>" /></p>
-
-		<p><label for="<?php echo $this->get_field_id( 'number' ); ?>"><?php _e( 'Number of comments to show:' ); ?></label>
-		<input id="<?php echo $this->get_field_id( 'number' ); ?>" name="<?php echo $this->get_field_name( 'number' ); ?>" type="text" value="<?php echo $number; ?>" size="3" /></p>
-<?php
-	}
 }
 
-function Register_Planet3_0_Recent_Comments() {  
-		register_widget( 'P3 Recent Comments' );  
-	}
+//register widget  
+function register_planet3_0_recent_comments_widget() {
+	register_widget( 'Planet3_0_Recent_Comments_Widget' );
+}
 
-add_action( 'widgets_init', 'Register_Planet3_0_Recent_Comments' );
+// load widget  
+add_action( 'widgets_init', 'register_planet3_0_recent_comments_widget' );
