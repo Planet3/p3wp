515c36f20b6e8d5fb8126ba1879f9265c7a0ae7c
Dan Moutal <dan@moutal.ca>
2013-11-03 18:34:43 -0800

    code clean up

--- content-fpcontent.php
+++ content-fpcontent.php
@@ -15,11 +15,9 @@
 			<?php if( $post->post_excerpt ) {
 				the_excerpt();
 			} else {
-				if ( ! is_feed() ) {
-					// Setting the global $more variable to 0 forces the content to be truncated at the <!--more--> tag
-					global $more;
-					$more = 0;
-				}
+				// Setting the global $more variable to 0 forces the content to be truncated at the <!--more--> tag
+				global $more;
+				$more = 0;
 				the_content( '<a class="moretag" href="'. get_permalink($post->ID) . '">[more]</a>' );
 			} ?>
 		</div><!-- .entry-summary -->
