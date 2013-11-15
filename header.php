7e0a768bc5ed393bee9bc910d73c9556b6820291
Michael Tobis <dan@moutal.ca>
2013-11-15 03:38:16 +0000

    remove header login button from sub-blogs

--- header.php
+++ header.php
@@ -24,28 +24,31 @@
 </head>
 
 <body <?php body_class(); ?>>
-	<?php if ( is_multisite() ) { switch_to_blog(1); } ?>
+	<?php if ( is_main_site() ) { ?>
 		<?php if (is_user_logged_in()) { ?>
-		<form id="login" action="login" method="post">
-			<a class="login_button" href="<?php echo wp_logout_url( home_url() ); ?>">Logout</a>
-		</form>
+			<form id="login" action="login" method="post">
+				<a class="login_button" href="<?php echo wp_logout_url( home_url() ); ?>">Logout</a>
+			</form>
 		<?php } else { ?>
-		<form id="login" action="login" method="post">
-			<a class="close" href="">[close]</a>
-			<h1>Planet3.0 Login</h1>
-			<p class="status"></p>
-			<label for="username">Username</label>
-			<input id="username" type="text" name="username">
-			<label for="password">Password</label>
-			<input id="password" type="password" name="password">
-			<a class="lost" href="<?php echo wp_lostpassword_url(); ?>">Lost your password?</a>
-			<input class="submit_button" type="submit" value="Login" name="submit">
-			<?php wp_nonce_field( 'ajax-login-nonce', 'security' ); ?>
-		</form>
+			<form id="login" action="login" method="post">
+				<a class="close" href="">[close]</a>
+				<h1>Planet3.0 Login</h1>
+				<p class="status"></p>
+				<label for="username">Username</label>
+				<input id="username" type="text" name="username">
+				<label for="password">Password</label>
+				<input id="password" type="password" name="password">
+				<a class="lost" href="<?php echo wp_lostpassword_url(); ?>">Lost your password?</a>
+				<input class="submit_button" type="submit" value="Login" name="submit">
+				<?php wp_nonce_field( 'ajax-login-nonce', 'security' ); ?>
+			</form>
 		<?php } ?>
+	<?php } ?>
+
 	<div id="page" class="hfeed site">
 		<?php do_action( 'before' ); ?>
 
+	<?php if ( is_multisite() ) { switch_to_blog(1); } ?>
 			<div class="top-bar-container antialiased">
 				<nav id="site-navigation" class="navigation-main top-bar" role="navivation">
 					<ul class="title-area">
@@ -56,21 +59,22 @@
 					</ul><!-- .title-area -->
 					<section class="top-bar-section">
 						<?php foundation_top_bar(); ?>
-						<ul class="right">
-							<li class="divider"></li>
-						<?php if (is_user_logged_in()) { ?>
-							<li class="has-dropdown"><a href="#">P3 Account</a>
-								<ul class="dropdown">
-									<?php if ( current_user_can( 'edit_posts' ) ) : ?><li><a href="<?php echo admin_url( 'post-new.php' ); ?>">Write an artile</a></li><?php endif ?>
-									<li><a href="<?php echo admin_url( 'profile.php' ); ?>">Edit Profile</a></li>
-	<?php if ( is_multisite() ) { restore_current_blog(); } ?>
-									<li class="has-form"><a class="login_button button" href="<?php echo wp_logout_url( home_url() ); ?>">Logout</a></li>
-								</ul>
-							</li>
-						<?php } else { ?>
-	<?php if ( is_multisite() ) { restore_current_blog(); } ?>
-							<li><a class="login_button" id="show_login" href="">Login</a></li>
-						<?php } ?>
+							<?php if ( is_multisite() ) { restore_current_blog(); } ?>
+							<ul class="right">
+								<?php if ( is_main_site() ) { ?>
+									<li class="divider"></li>
+								<?php if (is_user_logged_in()) { ?>
+									<li class="has-dropdown"><a href="#">P3 Account</a>
+										<ul class="dropdown">
+											<?php if ( current_user_can( 'edit_posts' ) ) : ?><li><a href="<?php echo admin_url( 'post-new.php' ); ?>">Write an artile</a></li><?php endif ?>
+											<li><a href="<?php echo admin_url( 'profile.php' ); ?>">Edit Profile</a></li>
+											<li class="has-form"><a class="login_button button" href="<?php echo wp_logout_url( home_url() ); ?>">Logout</a></li>
+										</ul>
+									</li>
+								<?php } else { ?>
+									<li><a class="login_button" id="show_login" href="">Login</a></li>
+								<?php } ?>
+							<?php } ?>
 							<li class="divider"></li>
 							<li class="has-form">
 								<form method="get" id="searchform" class="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
@@ -98,4 +102,3 @@
 
 
 	<div id="main" class="site-main">
-
